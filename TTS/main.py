1739281164f07e2cbbdb75b1872af1d85714d485
Hosein Nasiri NPC <hosein.nasiri@outlook.com>
2026-07-09 01:10:07 +0000

    Feature: TTS Inference code

---
+++ TTS/main.py
@@ -0,0 +1,29 @@
+#from TTS.api import TTS
+#tts=TTS(model_path="./best_model_91323.pth",config_path="./config.json")
+
+
+#tts.is_multi_lingual = False
+#tts.is_multi_speaker = False
+
+#tts.tts_to_file(".زندگی فقط یک بار است؛ از آن به خوبی استفاده کن",file_path='output.wav')
+
+
+from TTS.utils.synthesizer import Synthesizer
+
+synth = Synthesizer(
+    tts_checkpoint="./best_model_91323.pth",
+    tts_config_path="./config.json",
+)
+
+wav = synth.tts("زندگی فقط یک بار است؛ از آن به خوبی استفاده کن")
+
+synth.save_wav(wav, "output.wav")
+
+
+#from TTS.api import TTS
+
+#tts = TTS(model_path="./best_model_91323.pth",config_path="./config.json"
+#)
+
+#print("loaded")
+#print(tts.__dict__)
